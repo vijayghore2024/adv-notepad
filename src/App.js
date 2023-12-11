fe631b6bf20e526e3d736476e313eb7d641a56aa
vijayghore2024 <vijayghore2024@gmail.com>
2023-12-11 09:49:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Notepad from './components/Notepad';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div>
-        <NavBar />  
-        <Notepad />
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Notepad />} />   
+        <Route path='/about' element={<About />} />   
+        <Route path='/contact' element={<Contact />} />   
+      
+      </Routes>
+    </BrowserRouter>
   );
 }
 
