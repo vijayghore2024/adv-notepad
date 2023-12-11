ef3a50ff408d18251a656d9cfde6e6309c5a726b
vijayghore2024 <vijayghore2024@gmail.com>
2023-12-11 10:02:08 +0530

    third commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      This is about component
+    <div className='container text-center p-5 fs-1'>
+        
+        <p className="text-danger">This Advance notepad is created by using react</p>
+        <p className="text-warning">With this we also learn how to deploy project from github</p>
     </div>
   )
 }
